2a9e7dc9f18bbede65e63e6947397dcc266e3b3b
git <65435748@qq.com>
2019-01-17 11:03:26 +0800

    完成项目框架

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("欢迎进入位置管理系统！\n");
+    while (1)
+    {
+        printf("1---增加车辆位置\n");
+        printf("2---删除车辆位置\n");
+        printf("3---查看车辆位置\n");
+        printf("4---返回离我的位置最近的车辆\n");
+        printf("5---退出\n");
+
+        printf("请选择相应功能");
+        int code;
+        scanf("%d", code);
+
+        if (code = 1)
+        {
+        }
+        if (code = 2)
+        {
+        }
+        if (code = 3)
+        {
+        }
+        if (code = 4)
+        {
+        }
+        if (code = 5)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
